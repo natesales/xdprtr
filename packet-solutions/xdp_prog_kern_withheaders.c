2bdc98c561ee7e2787fe79d900f5964628159a68
Nate Sales <nate@natesales.net>
2021-05-02 15:38:28 -0700

    add router

---
+++ packet-solutions/xdp_prog_kern_withheaders.c
@@ -0,0 +1,160 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#include <linux/bpf.h>
+//#include <linux/in.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_endian.h>
+#include <arpa/inet.h>
+
+// The parsing helper functions from the packet01 lesson have moved here
+#include "../common/parsing_helpers.h"
+#include "../common/rewrite_helpers.h"
+
+/* Defines xdp_stats_map */
+#include "../common/xdp_stats_kern_user.h"
+#include "../common/xdp_stats_kern.h"
+
+#ifndef memcpy
+#define memcpy(dest, src, n) __builtin_memcpy((dest), (src), (n))
+#endif
+
+struct bpf_map_def SEC("maps") tx_port = {
+	.type = BPF_MAP_TYPE_DEVMAP,
+	.key_size = sizeof(int),
+	.value_size = sizeof(int),
+	.max_entries = 256,
+};
+
+struct bpf_map_def SEC("maps") redirect_params = {
+	.type = BPF_MAP_TYPE_HASH,
+	.key_size = ETH_ALEN,
+	.value_size = ETH_ALEN,
+	.max_entries = 1,
+};
+
+//#define AF_INET 2
+//#define AF_INET6 10
+#define IPV6_FLOWINFO_MASK bpf_htonl(0x0FFFFFFF)
+
+/* from include/net/ip.h */
+static __always_inline int ip_decrease_ttl(struct iphdr *iph) {
+	__u32 check = iph->check;
+	check += bpf_htons(0x0100);
+	iph->check = (__u16)(check + (check >= 0xFFFF));
+	return --iph->ttl;
+}
+
+// TODO: Remove
+//#define INET_ADDRSTRLEN		(16)
+//#define INET6_ADDRSTRLEN	(48)
+
+/* Solution to packet03/assignment-4 */
+SEC("xdp_router")
+int xdp_router_func(struct xdp_md *ctx) {
+	void *data_end = (void *)(long)ctx->data_end;
+	void *data = (void *)(long)ctx->data;
+	struct bpf_fib_lookup fib_params = {};
+	struct ethhdr *eth = data;
+	struct ipv6hdr *ip6h;
+	struct iphdr *iph;
+	__u16 h_proto;
+	__u64 nh_off;
+	int rc;
+	int action = XDP_PASS;
+
+	nh_off = sizeof(*eth);
+	if (data + nh_off > data_end) {
+		action = XDP_DROP;
+		goto out;
+	}
+
+	h_proto = eth->h_proto;
+	if (h_proto == bpf_htons(ETH_P_IP)) {
+		iph = data + nh_off;
+
+		if (iph + 1 > data_end) {
+			action = XDP_DROP;
+			goto out;
+		}
+
+		if (iph->ttl <= 1)
+			goto out;
+
+		fib_params.family	= AF_INET;
+		fib_params.tos		= iph->tos;
+		fib_params.l4_protocol	= iph->protocol;
+		fib_params.sport	= 0;
+		fib_params.dport	= 0;
+		fib_params.tot_len	= bpf_ntohs(iph->tot_len);
+		fib_params.ipv4_src	= iph->saddr;
+		fib_params.ipv4_dst	= iph->daddr;
+	} else if (h_proto == bpf_htons(ETH_P_IPV6)) {
+		struct in6_addr *src = (struct in6_addr *) fib_params.ipv6_src;
+		struct in6_addr *dst = (struct in6_addr *) fib_params.ipv6_dst;
+
+		ip6h = data + nh_off;
+		if (ip6h + 1 > data_end) {
+			action = XDP_DROP;
+			goto out;
+		}
+
+		if (ip6h->hop_limit <= 1)
+			goto out;
+
+		fib_params.family	= AF_INET6;
+		fib_params.flowinfo	= *(__be32 *) ip6h & IPV6_FLOWINFO_MASK;
+		fib_params.l4_protocol	= ip6h->nexthdr;
+		fib_params.sport	= 0;
+		fib_params.dport	= 0;
+		fib_params.tot_len	= bpf_ntohs(ip6h->payload_len);
+		*src			= ip6h->saddr;
+		*dst			= ip6h->daddr;
+	} else {
+		goto out;
+	}
+
+	fib_params.ifindex = ctx->ingress_ifindex;
+
+	rc = bpf_fib_lookup(ctx, &fib_params, sizeof(fib_params), 0);
+
+    char str[INET6_ADDRSTRLEN];
+//    struct in_addr ip_addr;
+//    ip_addr.s_addr = atoi(fib_params.ipv4_src);
+//    inet_ntop(AF_INET, (const void *)&ip_addr, str, INET6_ADDRSTRLEN);
+    inet_ntop(AF_INET, (const void *)&fib_params.ipv4_src, str, INET6_ADDRSTRLEN);
+	bpf_printk("src %s", str);
+	// bpf_printk("rc %d src %d dst %d", rc, fib_params.ipv4_src, fib_params.ipv4_dst);
+	switch (rc) {
+	case BPF_FIB_LKUP_RET_SUCCESS:         /* lookup successful */
+		if (h_proto == bpf_htons(ETH_P_IP))
+			ip_decrease_ttl(iph);
+		else if (h_proto == bpf_htons(ETH_P_IPV6))
+			ip6h->hop_limit--;
+
+		memcpy(eth->h_dest, fib_params.dmac, ETH_ALEN);
+		memcpy(eth->h_source, fib_params.smac, ETH_ALEN);
+		action = bpf_redirect_map(&tx_port, fib_params.ifindex, 0);
+		break;
+	case BPF_FIB_LKUP_RET_BLACKHOLE:    /* dest is blackholed; can be dropped */
+	case BPF_FIB_LKUP_RET_UNREACHABLE:  /* dest is unreachable; can be dropped */
+	case BPF_FIB_LKUP_RET_PROHIBIT:     /* dest not allowed; can be dropped */
+		action = XDP_DROP;
+		break;
+	case BPF_FIB_LKUP_RET_NOT_FWDED:    /* packet is not forwarded */
+	case BPF_FIB_LKUP_RET_FWD_DISABLED: /* fwding is not enabled on ingress */
+	case BPF_FIB_LKUP_RET_UNSUPP_LWT:   /* fwd requires encapsulation */
+	case BPF_FIB_LKUP_RET_NO_NEIGH:     /* no neighbor entry for nh */
+	case BPF_FIB_LKUP_RET_FRAG_NEEDED:  /* fragmentation required to fwd */
+		/* PASS */
+		break;
+	}
+
+out:
+	return xdp_stats_record_action(ctx, action);
+}
+
+SEC("xdp_pass")
+int xdp_pass_func(struct xdp_md *ctx) {
+	return XDP_PASS;
+}
+
+char _license[] SEC("license") = "GPL";
